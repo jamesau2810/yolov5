a952c0cace09791909c47731e975935a0bd19c93
James <pmau@connect.ust.hk>
2024-05-02 14:45:31 +0800

    item

--- nuc_usb_test.py
+++ nuc_usb_test.py
@@ -41,9 +41,10 @@ if __name__ == '__main__':
     # serialObj.flush()
     print(ied)
     # arr = []
-    while serialObj.available() > 0:
-        # arr.append(serialObj.read())
-        print(serialObj.read())
+    print(serialObj.readline())
+    # while serialObj.available() > 0:
+    #     # arr.append(serialObj.read())
+    #     print(serialObj.read())
     # print(arr)
     # # x = 0
     # # while x< 12:
