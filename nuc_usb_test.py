548ddce682b003ad292cc6e448cb87a3e0de80ec
James <pmau@connect.ust.hk>
2024-05-03 15:20:45 +0800

    DEMO

--- nuc_usb_test.py
+++ nuc_usb_test.py
@@ -1,45 +1,45 @@
-"""
-# Python code transmits a byte to Arduino /Microcontroller
-import serial
-import time
-path = '/dev/ttyACM0'
-# path = '/dev/cu.usbmodem141301'
+# """
+# # Python code transmits a byte to Arduino /Microcontroller
+# import serial
+# import time
+# path = '/dev/ttyACM0'
+# # path = '/dev/cu.usbmodem141301'
 
 
-if __name__ == '__main__':
-    # SerialObj = serial.Serial(path) # COMxx  format on Windows
-    #                   # ttyUSBx format on Linux COM24 /dev/cu.usbmodem141301 /dev/ttyACM0
+# if __name__ == '__main__':
+#     # SerialObj = serial.Serial(path) # COMxx  format on Windows
+#     #                   # ttyUSBx format on Linux COM24 /dev/cu.usbmodem141301 /dev/ttyACM0
 
-    # SerialObj.baudrate = 9600
-    # SerialObj.parity  ='N'   # No parity
-    # SerialObj.stopbits = 1   # Number of Stop bits = 1
-    # time.sleep(3)
-    # SerialObj.write(b'A')    #transmit 'A' (8bit) to micro/Arduino
-    # print(SerialObj.read(size=1))
-    # SerialObj.close()      # Close the port
-    # 150070240160
-    serialObj = serial.Serial(path,timeout=5)
-    # serialObj.baudrate = 9600  # set Baud rate to 9600
-    # SerialObj.bytesize = 8   # Number of data bits = 8
-    left = 150
-    up = 120 
-    width_x = 240
-    width_y = 160
-    # SendItem="#"+str(left)+"&"+str(up)+"&"+str(width_x)+"&"+str(width_y)+"%"
-    # "150070240160"
-    time.sleep(3)
-    # enc =  SendItem.encode('')
-    # print(enc)
-    # # Write data to the USB port
-    # serialObj.write( b'Hello, World!')
-    ArduinoSent(left,up,width_x,width_y,serialObj)
-    # serialObj.flush()
-    # print(ied)
-    print(serialObj.read(size=12))
-    # # x = 0
-    # # while x< 12:
+#     # SerialObj.baudrate = 9600
+#     # SerialObj.parity  ='N'   # No parity
+#     # SerialObj.stopbits = 1   # Number of Stop bits = 1
+#     # time.sleep(3)
+#     # SerialObj.write(b'A')    #transmit 'A' (8bit) to micro/Arduino
+#     # print(SerialObj.read(size=1))
+#     # SerialObj.close()      # Close the port
+#     # 150070240160
+#     serialObj = serial.Serial(path,timeout=5)
+#     # serialObj.baudrate = 9600  # set Baud rate to 9600
+#     # SerialObj.bytesize = 8   # Number of data bits = 8
+#     left = 150
+#     up = 120 
+#     width_x = 240
+#     width_y = 160
+#     # SendItem="#"+str(left)+"&"+str(up)+"&"+str(width_x)+"&"+str(width_y)+"%"
+#     # "150070240160"
+#     time.sleep(3)
+#     # enc =  SendItem.encode('')
+#     # print(enc)
+#     # # Write data to the USB port
+#     # serialObj.write( b'Hello, World!')
+#     ArduinoSent(left,up,width_x,width_y,serialObj)
+#     # serialObj.flush()
+#     # print(ied)
+#     print(serialObj.read(size=12))
+#     # # x = 0
+#     # # while x< 12:
     
-"""
+# """
 
 
 
@@ -108,7 +108,7 @@ vehicle.mode = VehicleMode(modeUsed)
 # while True:
 #     print(library.checklocation(vehicle))
 library.instr_2_takeoff(vehicle,modeUsed,altitude=5)
-library.waypoint(vehicle,223349175,1142630053,10)
+# library.waypoint(vehicle,223349175,1142630053,10)
 
 #
 
